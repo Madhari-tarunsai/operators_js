7ab13cc291385c9b9212370fa925ae0e7b13c304
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2024-12-19 22:24:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+alert(`Now we will check the length of two numbers , which  are ( Greater ) or ( Equal ) or ( Smaller )`);
+      let l1=prompt("enter the value of l1:")
+      let l2=prompt("enter the value of l2:")
+
+      let result;
+      if(l1>l2){
+        alert(`greater`)
+      }
+      else(l1<l2)
+      {
+        alert(`smaller`)
+      }
+      if(l1==l2)
+        {
+            alert(`equal`)
+      }
